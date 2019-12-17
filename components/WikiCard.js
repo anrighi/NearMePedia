fb6c7142ceef40c0d11ab171fde8550aa9bcddcc
cristiano <clucian@unibz.it>
2019-12-17 22:58:49 +0100

    Wiki component list + nav

--- components/WikiCard.js
+++ components/WikiCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {ScrollView, StyleSheet, Image, Text, View, Button} from 'react-native';
+import {ScrollView, TouchableOpacity, StyleSheet, Image, Text, View, Button} from 'react-native';
+import * as WebBrowser from "expo-web-browser";
 
 export default class WikiCard extends React.Component {
 
@@ -12,13 +13,43 @@ export default class WikiCard extends React.Component {
         coord: this.props.coord
     }
 
-
-    render() {
-        return (
-            <View>
-                <Text>{this.state.title}</Text>
-            </View>
+    handlePress(title){
+        let link = 'https://en.wikipedia.org/wiki/' + title;
+        WebBrowser.openBrowserAsync(
+            link
         );
     }
 
+    render() {
+        return (
+            <View style={styles.container}>
+                <TouchableOpacity style={styles.touchable} onPress={async () => await this.handlePress(this.state.title)}>
+                    <Text>
+                        {this.state.title}
+                    </Text>
+                    <Button
+                        title="♡"
+                        style={styles.button}
+                    />
+                </TouchableOpacity>
+            </View>
+        );
+    }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 10,
+    },
+    touchable: {
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 10,
+
+    },
+    button: {
+        color: 'green',
+
+    }
+});
